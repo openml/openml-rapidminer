d4dd40876631fd0ac03f0236b1d2ea391977a54f
ArlindKadra <arlindkadra@gmail.com>
2017-11-28 14:26:53 +0100

    Updating project functionality

--- Experiment/src/org/openml/experiment/Experiment.java
+++ Experiment/src/org/openml/experiment/Experiment.java
@@ -3,6 +3,7 @@ package org.openml.experiment;
 import java.io.File;
 import java.net.URL;
 import java.util.HashMap;
+import java.util.Map.Entry;
 
 import org.apache.commons.io.FileUtils;
 import org.apache.commons.lang3.exception.ExceptionUtils;
@@ -13,54 +14,58 @@ import org.openml.apiconnector.xml.Flow;
 import org.openml.apiconnector.xml.SetupParameters;
 import org.openml.apiconnector.xml.SetupParameters.Parameter;
 
+import com.rapidminer.Process;
+import com.rapidminer.RapidMiner;
+import com.rapidminer.operator.Operator;
+
 public class Experiment {
 	
-	private int taskId;
+	private String taskId;
 	private int flowId;
 	private int setupId;
 	private String url = null;
 	private String key = null;
 	private Logger logger;
-	private Config config = null;
 	private HashMap<String, HashMap<String, String>> parameters;
 	
-	public Experiment(HashMap<String, String> parameters) {
+	public Experiment(HashMap<String, String> parameters) throws Exception{
 		
 		logger = Logger.getInstance();
 		
 		try {
-			this.taskId = Integer.parseInt(parameters.get("taskId"));
-			this.flowId = Integer.parseInt(parameters.get("flowId"));
-			this.setupId = Integer.parseInt(parameters.get("setupId"));
+			this.taskId = parameters.get("task_id");
+			this.flowId = Integer.parseInt(parameters.get("flow_id"));
+			this.setupId = Integer.parseInt(parameters.get("setup_id"));
 			
 			if(parameters.containsKey("url") && parameters.containsKey("key")) {
 				this.url = parameters.get("url");
 				this.key = parameters.get("key");
 			}
 			else {
-				config = getConfigurationFile();
+				Config config = getConfigurationFile();
+				this.url = config.getServer();
+				this.key = config.getApiKey();
 			}
+			this.build();
 		}
 		catch(NumberFormatException e) {
 			logger.logToFile(e.getMessage() + ExceptionUtils.getStackTrace(e));
 			throw e;
 		}
 		catch(Exception e) {
-			
+			logger.logToFile(e.getMessage() + ExceptionUtils.getStackTrace(e));
+			throw e;
 		}
 	}
 	
-	private void build() {
+	/** Build the experiment by configuring the operators with their corresponding parameters.
+	 * 
+	 * @throws Exception
+	 */
+	private void build() throws Exception{
 		
-		
-		OpenmlConnector connector;
 		File processFile;
-		if(config != null) {
-			connector = new OpenmlConnector(config.getServer(), config.getApiKey());
-		}
-		else {
-			connector = new OpenmlConnector(this.url, this.key);
-		}
+		OpenmlConnector connector = new OpenmlConnector(this.url, this.key);
 		
 		try {
 			processFile = getTheProcessFile(this.flowId, connector);
@@ -68,11 +73,51 @@ public class Experiment {
 			this.parameters = groupParameters(paramFile);
 		}
 		catch(Exception e) {
-			
+			logger.logToFile(e.getMessage() + ExceptionUtils.getStackTrace(e));
+			throw e;
 		}
+		RapidMiner.setExecutionMode(RapidMiner.ExecutionMode.COMMAND_LINE);
+		RapidMiner.init();
+		Process process = RapidMiner.readProcessFile(processFile);
+		processFile.deleteOnExit();
+		configureDownloadOp(process, this.url, this.key, this.taskId);
+		configureUploadOp(process, this.url, this.key);
+		configureOtherOp(process, this.parameters);
+		
+		process.run();
 
 	}
 	
+	private void configureDownloadOp(Process process, String url, String key, String taskId ) {
+
+		Operator download = process.getOperator("Download");           
+		download.setParameter("Url", url);
+		download.setParameter("Api key", key);
+		download.setParameter("Task_id", taskId);
+	}
+	
+	private void configureUploadOp(Process process, String url, String key) {
+
+		Operator download = process.getOperator("Upload");           
+		download.setParameter("Url", url);
+		download.setParameter("Api key", key);
+	}
+	
+	private void configureOtherOp(Process process, HashMap<String, HashMap<String, String>> param) {
+		
+		for (String operatorName : param.keySet()) {
+			Operator operator = process.getOperator(operatorName);
+			for (Entry<String, String> entry : param.get(operatorName).entrySet()) {
+			    operator.setParameter(entry.getKey(), entry.getValue());
+			}
+		}
+	}
+	
+	/**
+	 * Groups the parameter names and values on a HashMap. The HashMaps are then grouped on operators in another HashMap.
+	 * @param paramFile 
+	 * @return
+	 */
 	private HashMap<String, HashMap<String, String>> groupParameters(SetupParameters paramFile) {
 		
 		HashMap<String, HashMap<String, String>> parameters = new HashMap<String, HashMap<String, String>>();
@@ -88,11 +133,22 @@ public class Experiment {
 				parameters.put(operator, temp);
 			}
 			else {
-				parameters.put(operator, new HashMap<String, String>());
+				HashMap<String, String> temp = new HashMap<String, String>();
+				temp.put(paramName, value);
+				parameters.put(operator, temp);
+				
 			}
 		}
 		return parameters;
 	}
+	
+	/**
+	 * Uses the processUrl field from the flow and downloads the process file.
+	 * @param flowId
+	 * @param connector
+	 * @return RapidMiner Process file
+	 * @throws Exception
+	 */
 	
 	private File getTheProcessFile(int flowId, OpenmlConnector connector) throws Exception {
 		
--- Experiment/src/org/openml/experiment/Main.java
+++ Experiment/src/org/openml/experiment/Main.java
@@ -1,9 +1,21 @@
 package org.openml.experiment;
 
+import java.util.HashMap;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		HashMap<String, String> map = new HashMap<String, String>();
+		map.put("setup_id", "1088");
+		map.put("task_id", "115");
+		map.put("flow_id", "4682");
+		try {
+			new Experiment(map);
+		}
+		catch(Exception e) {
+			
+		}
 		
 	}
 
